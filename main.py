c030f19090e7bd96ca552e66e6c3ac4765ecf817
Samuel Carvalho <115167862+Rekyeme@users.noreply.github.com>
2023-01-18 23:04:17 +0000

    Final Game

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import pygame
 import sys
 import time
 from settings import *
-from sprites import BG
+from sprites import BG, Ground, Plane, Obstacle
 
 
 class Program:
 
     # Display and Clock setup;
     def __init__(self):
+        self.active = None
         pygame.init()
         self.display_surface = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("")
@@ -19,11 +20,27 @@ class Program:
         self.collision_sprites = pygame.sprite.Group()
 
         # Scaling Factor
-        bg_HEIGHT = pygame.image.load("../IMFJ1---Laboratorio-de-Pygame-Projeto-Final/Assets/background.png").get_height()
+        bg_HEIGHT = pygame.image.load(
+            "../IMFJ1---Laboratorio-de-Pygame-Projeto-Final/Assets/background.png").get_height()
         self.scaling_factor = HEIGHT / bg_HEIGHT
 
         # Sprite Configuration
         BG(self.all_sprites, self.scaling_factor)
+        Ground([self.all_sprites, self.collision_sprites], self.scaling_factor)
+        self.Plane = Plane(self.all_sprites, self.scaling_factor * 2)
+
+        # Timer
+        self.obstacle_timer = pygame.USEREVENT + 1
+        pygame.time.set_timer(self.obstacle_timer, 1400)
+
+    def collisions(self):
+        if pygame.sprite.spritecollide(self.Plane, self.collision_sprites, False, pygame.sprite.collide_mask) \
+                or self.Plane.rect.top <= 0:
+            for sprite in self.collision_sprites.sprites():
+                if sprite.sprite_type == "  obstacle":
+                    sprite.kill()
+            self.active = False
+            self.Plane.kill()
 
     # Program main loop;
     def run(self):
@@ -37,10 +54,16 @@ class Program:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYUP:
+                    self.Plane.Jump()
+
+                if event.type == self.obstacle_timer:
+                    Obstacle([self.all_sprites, self.collision_sprites], self.scaling_factor * 1.2)
 
             # Updates the display and limits the frame-rate;
             self.display_surface.fill("black")
             self.all_sprites.update(dt)
+            self.collisions()
             self.all_sprites.draw(self.display_surface)
 
             pygame.display.update()
--- sprites.py
+++ sprites.py
@@ -1,5 +1,6 @@
 import pygame
 from settings import *
+from random import choice, randint
 
 
 class BG(pygame.sprite.Sprite):
@@ -23,3 +24,110 @@ class BG(pygame.sprite.Sprite):
         if self.rect.centerx <= 0:
             self.pos.x = 0
         self.rect.x = round(self.pos.x)
+
+
+class Ground(pygame.sprite.Sprite):
+    def __init__(self, groups, scaling_factor):
+        super().__init__(groups)
+
+        # Image import and scaling;
+        ground_surface = pygame.image.load(
+            "../IMFJ1---Laboratorio-de-Pygame-Projeto-Final/Assets/ground.png").convert_alpha()
+        assert isinstance(scaling_factor, object)
+        self.image = pygame.transform.scale(ground_surface,
+                                            pygame.math.Vector2(ground_surface.get_size()) * scaling_factor)
+
+        # Position set;
+        self.rect = self.image.get_rect(bottomleft=(0, HEIGHT))
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+
+        # Mask
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def update(self, dt):
+        self.pos.x -= 360 * dt
+        if self.rect.centerx <= 0:
+            self.pos.x = 0
+
+        self.rect.x = round(self.pos.x)
+
+
+class Plane(pygame.sprite.Sprite):
+    def __init__(self, groups, scaling_factor):
+        super().__init__(groups)
+
+        # Image import and scaling;
+        self.mask = None
+        self.import_frames(scaling_factor)
+        self.frame_index = 0
+        self.image = self.frames[self.frame_index]
+
+        # Rect
+        self.rect = self.image.get_rect(midleft=(WIDTH / 20, HEIGHT / 2))
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+
+        # Movement
+        self.gravity = 484
+        self.direction = 0
+
+    def import_frames(self, scaling_factor):
+        self.frames = []
+        for i in range(2):
+            surf = pygame.image.load(
+                f'../IMFJ1---Laboratorio-de-Pygame-Projeto-Final/Assets/Plane{i}.png').convert_alpha()
+            scaled_surface = pygame.transform.scale(surf, pygame.math.Vector2(surf.get_size()) * scaling_factor)
+            self.frames.append(scaled_surface)
+
+    def applied_gravity(self, dt):
+        self.direction += self.gravity * dt
+        self.pos.y += self.direction * dt
+        self.rect.y = round(self.pos.y)
+
+    def Jump(self):
+        self.direction = - 242
+
+    def animate(self, dt):
+        self.frame_index += 21 * dt
+        if self.frame_index >= len(self.frames):
+            self.frame_index = 0
+        self.image = self.frames[int(self.frame_index)]
+
+    def rotate(self, dt):
+        rotated_plane = pygame.transform.rotozoom(self.image, -self.direction * 0.10, 1)
+        self.image = rotated_plane     
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def update(self, dt):
+        self.applied_gravity(dt)
+        self.animate(dt)
+        self.rotate(dt)
+
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, groups, scaling_factor):
+        super().__init__(groups)
+
+        orientation = choice(("up", "down"))
+        surf = pygame.image.load(f"../IMFJ1---Laboratorio-de-Pygame-Projeto-Final/Assets/{choice((0, 1))}.png").convert_alpha()
+        self.image = pygame.transform.scale(surf, pygame.math.Vector2(surf.get_size()) * scaling_factor)
+
+        x = WIDTH + randint(40, 100)
+
+        if orientation == 'up':
+            y = HEIGHT + randint(10, 50)
+            self.rect = self.image.get_rect(midbottom=(x, y))
+        else:
+            y = randint(-50, -10)
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect = self.image.get_rect(midtop=(x, y))
+
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+
+        # mask
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def update(self, dt):
+        self.pos.x -= 400 * dt
+        self.rect.x = round(self.pos.x)
+        if self.rect.right <= -100:
+            self.kill()
